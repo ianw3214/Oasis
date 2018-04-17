b3f93b49c5c168fde289a2b68911a5201d9ab053
Ian <ianw3214@gmail.com>
2018-04-17 15:31:24 -0400

    Implement tilemap class

---
+++ src/graphics/tilemap.cpp
@@ -0,0 +1,48 @@
+#include "tilemap.hpp"
+
+#include "../core/engine.hpp"
+#include "../util/util.hpp"
+
+TileMap::TileMap(SDL_Texture * const texture) : Texture(texture) {
+
+}
+
+TileMap::TileMap(const std::string& path) : Texture(path) {
+
+}
+
+TileMap::~TileMap() {
+
+}
+
+void TileMap::generateTiles(int tile_width, int tile_height) {
+	for (int i = 0; i < (height / tile_height); ++i) {
+		for (int j = 0; j < (width / tile_width); ++j) {
+			tiles.push_back(Math::Rectangle(j * tile_width, i * tile_height, tile_width, tile_height));
+		}
+	}
+}
+
+void TileMap::render(int tile_num) const {
+	render(0, 0, tile_num);
+}
+
+void TileMap::render(int x, int y, int tile_num) const {
+	ASSERT(QcEngine::getRenderer());
+	if (!texture) {
+		ERR("Tried to render a NULL texture...");
+		return;
+	}
+	// print a warning if there's no rect set for the specified tile
+	if (static_cast<unsigned int>(tile_num) >= tiles.size()) {
+		ERR("TILE: " << tile_num << " IS NOT SPECIFIED IN THE TILEMAP");
+		tile_num = 0;
+	}
+	SDL_Rect src = { tiles[tile_num].pos.x, tiles[tile_num].pos.y, tiles[tile_num].w, tiles[tile_num].h };
+	if (fullscreen) {
+		SDL_RenderCopyEx(QcEngine::getRenderer(), texture, &src, nullptr, angle, &centre, flip);
+	} else {
+		SDL_Rect target = { x, y, tiles[tile_num].w, tiles[tile_num].h };
+		SDL_RenderCopyEx(QcEngine::getRenderer(), texture, &src, &target, angle, &centre, flip);
+	}
+}
