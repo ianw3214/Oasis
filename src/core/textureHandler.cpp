2c97d8d2af91cd2e1aba35d2327ab20dcc034152
Ian <ianw3214@gmail.com>
2018-04-22 16:24:28 -0400

    Implement texture handler system
    Associates textures with IDs that are now managed by the engine instead of relying on the user to handle raw textures

---
+++ src/core/textureHandler.cpp
@@ -0,0 +1,38 @@
+#include "textureHandler.hpp"
+
+TextureHandler::TextureHandler() {
+
+}
+
+TextureHandler::~TextureHandler() {
+	for (auto& a : dict) {
+		delete a.second;
+	}
+}
+
+Texture * TextureHandler::getTexture(std::string id) {
+	if (dict.find(id) == dict.end()) {
+		return nullptr;
+	}
+	return dict[id];
+}
+
+Texture * TextureHandler::addTexture(std::string id, Texture * tex) {
+	if (dict.find(id) != dict.end()) {
+		delete dict[id];
+	}
+	return dict[id] = tex;
+}
+
+Texture * TextureHandler::addTexture(std::string id, std::string path, TextureType t) {
+	if (dict.find(id) != dict.end()) {
+		delete dict[id];
+	}
+	if (t == T_DEFAULT) {
+		dict[id] = new Texture(path);
+	}
+	if (t == T_ANIMATED) {
+		dict[id] = new AnimatedTexture(path);
+	}
+	return dict[id];
+}
---
+++ src/core/textureHandler.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "texture.hpp"
+#include "../graphics/animatedTexture.hpp"
+
+#include <map>
+#include <string>
+
+enum TextureType {
+	T_DEFAULT,
+	T_ANIMATED
+};
+
+class TextureHandler {
+
+public:
+	TextureHandler();
+	~TextureHandler();
+	TextureHandler(const TextureHandler&) = delete;
+	TextureHandler operator=(const TextureHandler&) = delete;
+
+	Texture* getTexture(std::string id);
+	Texture* addTexture(std::string id, Texture* tex);
+	Texture* addTexture(std::string id, std::string path, TextureType t = T_DEFAULT);
+
+private:
+	std::map<std::string, Texture*> dict;
+};
